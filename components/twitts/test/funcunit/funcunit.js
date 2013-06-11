10489353fc5bb93f6fff7377c5fdd86c6ffb4b60
dimaa <dimaa@ubuntu.(none)>
2013-06-11 20:32:34 +0400

    callback functions rewrite

--- components/twitts/test/funcunit/funcunit.js
+++ components/twitts/test/funcunit/funcunit.js
@@ -22,24 +22,28 @@ steal('components/twitts',
                 equals(S('.table tbody tr').size(), 5, "there are 5 results in the table")
             })
             S.wait(500)
-            S('.reload').click()
-            S('.table').visible(function(){
+            S('.reload').click(function(){
+                S('.table').visible(function(){
                 equals(S('.table tbody tr').size(), 5, "there are 5 results in the table")
             })
+            })
+            
         })
 
         test('search_table_check', function(){
             S('.input').type("sdf")
-            S('.searchBtn').click()
-            S.wait(1000)
-            S('.table').visible(function(){
-                equals(S('.table tbody tr').size(), 4, "there are should be 4 results in the seatch table")
-                for (i = 0; i < S('.table tbody tr').size(); i++) {  
-                    var a = S('.table tbody tr:eq('+i+') td:eq(2)').html()
-                    a = a.replace(/\s+/g,'');        
-                    equal(a, "sdf"+i)
-                } 
+            S('.searchBtn').click(function() {
+                S.wait(1000)
+                S('.table').visible(function(){
+                    equals(S('.table tbody tr').size(), 4, "there are should be 4 results in the seatch table")
+                    for (i = 0; i < S('.table tbody tr').size(); i++) {  
+                        var a = S('.table tbody tr:eq('+i+') td:eq(2)').html()
+                        a = a.replace(/\s+/g,'');        
+                        equal(a, "sdf"+i)
+                    } 
+                })
             })
+            
         })
       
         test('clear_table_check', function(){
